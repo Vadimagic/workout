133c72b34ced0e54ab0aaf24cedb8396416fa322
Vadim Bida <vadim.prog.2000@gmail.com>
2023-03-05 22:43:46 +1000

    add middleware | prisma | auth methods

--- server/app/auth/auth.controller.js
+++ server/app/auth/auth.controller.js
@@ -1,8 +1,67 @@
-// @desc 		Auth user
-// @route 	POST /api/users/login
-// @access 	public
-export const authUser = (req, res) => {
-	res.json({
-		message: 'You are authenticated'
+import { faker } from '@faker-js/faker'
+import { hash, verify } from 'argon2'
+import asyncHandler from 'express-async-handler'
+
+import { prisma } from '../prisma.js'
+import { UserFields } from '../utils/users.utils.js'
+
+import { generateToken } from './generate-token.js'
+
+/*
+	@desc 		Auth user
+	@route 		POST /api/users/login
+	@access 	public
+*/
+export const authUser = asyncHandler(async (req, res) => {
+	const { email, password } = req.body
+	const user = await prisma.user.findUnique({
+		where: { email }
 	})
-}
+
+	if (!user) {
+		res.status(400)
+		throw new Error('User not found')
+	}
+
+	const isVerifyPassword = await verify(user.password, password)
+
+	if (!isVerifyPassword) {
+		res.status(400)
+		throw new Error('Incorrect email or password')
+	}
+
+	const token = generateToken(user.id)
+
+	res.json({ token })
+})
+
+/*
+	@desc 		Register user
+	@route 		POST /api/users/register
+	@access 	public
+*/
+export const registerUser = asyncHandler(async (req, res) => {
+	const { email, password } = req.body
+
+	const isHaveUser = await prisma.user.findUnique({
+		where: { email }
+	})
+
+	if (isHaveUser) {
+		res.status(400)
+		throw new Error('User already exists')
+	}
+
+	const user = await prisma.user.create({
+		data: {
+			email,
+			password: await hash(password),
+			name: faker.name.fullName()
+		},
+		select: UserFields
+	})
+
+	const token = generateToken(user.id)
+
+	res.json({ user, token })
+})
--- server/server.js
+++ server/server.js
@@ -3,13 +3,20 @@ import * as dotenv from 'dotenv'
 import express from 'express'
 import morgan from 'morgan'
 
+import { errorHandler, notFound } from './app/middleware/error.middleware.js'
+
 import authRoutes from './app/auth/auth.routes.js'
+import { prisma } from './app/prisma.js'
+
+/*
+	TODO:
+	[] - Async error handling for method
+	[] - App.use notFound, errorHandler
+*/
 
 dotenv.config()
 
 const app = express()
-
-const PORT = process.env.PORT || 5000
 
 async function main() {
 	if (process.env.NODE_ENV === 'development') {
@@ -18,6 +25,11 @@ async function main() {
 	app.use(express.json())
 	app.use('/api/auth', authRoutes)
 
+	// middleware
+	app.use(notFound)
+	app.use(errorHandler)
+
+	const PORT = process.env.PORT || 5000
 	app.listen(
 		PORT,
 		console.log(
@@ -28,3 +40,11 @@ async function main() {
 }
 
 main()
+	.then(async () => {
+		await prisma.$disconnect()
+	})
+	.catch(async e => {
+		console.error(e)
+		await prisma.$disconnect
+		process.exit()
+	})
